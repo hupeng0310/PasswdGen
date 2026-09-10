019027cfd7ec0ca2dce6935696a45c75bd2b944b
胡鹏 <hupgwuty@163.com>
2026-09-10 17:48:39 +0800

    fix: windows平台乱码问题处理

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 #include "cli.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main(int argc, char *argv[]) {
+#ifdef _WIN32
+    // Windows 控制台默认使用 GBK（代码页936），切换为 UTF-8 以正确显示中文输出
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
     int result = parse_and_execute(argc, argv);
     
     // 返回码: 0=成功, 1=显示帮助, -1=错误
